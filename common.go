c2f5ef7196cd272cb845380ac968586fc1df6bc2
Can Yucel <can@koding.com>
2014-10-28 01:10:09 -0700

    main: move prepareArgs to common

--- common.go
+++ common.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -48,4 +49,47 @@ func runCommand(name string, args ...string) (*exec.Cmd, error) {
 	go io.Copy(os.Stderr, stderr)
 
 	return cmd, nil
+}
+
+func prepareArgs() *Params {
+
+	params := NewParams()
+
+	args := os.Args
+
+	// remove command
+	args = args[1:len(args)]
+
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		arg = stripDash(arg)
+
+		if arg == "run" || arg == "watch" {
+			if len(args) <= i+1 {
+				log.Fatalf("missing parameter value: %s", arg)
+			}
+
+			params.System[arg] = args[i+1]
+			i++
+			continue
+		}
+
+		params.Package = append(params.Package, args[i])
+	}
+
+	params.CloneRun()
+
+	return params
+}
+
+func stripDash(arg string) string {
+	if len(arg) > 2 {
+		if arg[1] == '-' {
+			return arg[2:]
+		} else if arg[0] == '-' {
+			return arg[1:]
+		}
+	}
+
+	return arg
 }
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import "os"
-
 const binaryName = "goldorf-main"
 
 func main() {
-	args, rootpackage := prepareArgs()
+	params := prepareArgs()
 
 	w := MustRegisterWatcher(rootpackage)
 	defer w.Close()
@@ -26,25 +24,4 @@ func main() {
 	go w.ListenChanges()
 
 	<-done
-}
-
-func prepareArgs() ([]string, string) {
-	args := os.Args
-
-	// remove command
-	args = args[1:len(args)]
-
-	filteredArgs := make([]string, 0)
-	var rootpackage string
-	// !!! TODO change this part, it is error prone for parameters w/o values.
-	for i := 0; i < len(args); i += 2 {
-		if args[i] == "-rootpackage" || args[i] == "--rootpackage" {
-			rootpackage = args[i+1]
-			continue
-		}
-
-		filteredArgs = append(filteredArgs, args[i], args[i+1])
-	}
-
-	return filteredArgs, rootpackage
 }
