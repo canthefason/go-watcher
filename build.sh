ececf27f293a157dee0c656337693d838e66f792
Jon Yucel <jon.yucel@getcruise.com>
2018-02-22 00:00:02 -0800

    fix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -x
 GO_VERSION=$(go version | awk '{print $3}')
 
-/bin/bash -c "docker build -f Dockerfiles/Dockerfile.$GO_VERSION" -t "canthefason/go-watcher:$WATCHER_VERSION-$GO_VERSION" .
+docker build -f Dockerfiles/Dockerfile.$GO_VERSION -t canthefason/go-watcher:$WATCHER_VERSION-$GO_VERSION .
 
 docker push canthefason/go-watcher:$WATCHER_VERSION-$GO_VERSION
