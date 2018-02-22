c22424336ca50e8635faea46422615c5613d1dc2
Jon Yucel <jon.yucel@getcruise.com>
2018-02-22 00:11:53 -0800

    branch protection

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -xe
-GO_VERSION=$(go version | awk '{print $3}')
+#if [ "$TRAVIS_BRANCH" == "master" ]; then
+  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+  GO_VERSION=$(go version | awk '{print $3}')
 
-docker build -f Dockerfiles/Dockerfile.$GO_VERSION -t canthefason/go-watcher:$WATCHER_VERSION-$GO_VERSION .
+  docker build -f Dockerfiles/Dockerfile.$GO_VERSION -t canthefason/go-watcher:$WATCHER_VERSION-$GO_VERSION .
 
-docker push canthefason/go-watcher:$WATCHER_VERSION-$GO_VERSION
+  docker push canthefason/go-watcher:$WATCHER_VERSION-$GO_VERSION
+#fi
